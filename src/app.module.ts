731fd6e92b67abae80eb354a00dc9dd19717d383
teste-code <codeweb39@gmail.com>
2024-05-15 14:42:03 +0000

    guiure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SoundsModule } from './sounds/sounds.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [SoundsModule],
+  imports: [SoundsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
